f7f39c3c3ec125d1d3869cb66135a643a0cdfbb9
Ambert Jean-Christophe <j.ambert@free.fr>
2014-12-27 16:39:38 +0100

    integrate WebConfigurationManager for ASP

---
+++ MongoRepository/IConfigurationManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+
+namespace MongoRepository
+{
+    public interface IConfigurationManager
+    {
+        object GetSection(string sectionName);
+    }
+
+    public class ExeConfigurationManager : IConfigurationManager
+    {
+
+       private static Configuration configuration_;
+
+       static ExeConfigurationManager()
+        {
+            configuration_ = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+        }
+
+        public object GetSection(string sectionName)
+        {
+            return configuration_.GetSection(sectionName);
+        }
+    }
+
+    public class AspConfigurationManager : IConfigurationManager
+    {
+
+        static AspConfigurationManager()
+        {
+         
+        }
+
+        public object GetSection(string sectionName)
+        {
+            return WebConfigurationManager.GetSection(sectionName);
+        }
+    }
+
+    public static class ConfigurationManagerBuilder
+    {
+        public static IConfigurationManager Build()
+        {
+            var configFile = Path.GetFileName(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            if (configFile.ToLower().Equals("web.config"))
+                return new AspConfigurationManager();
+            return new ExeConfigurationManager();
+        }
+    }
+}
--- MongoRepository/MongoConfiguration.cs
+++ MongoRepository/MongoConfiguration.cs
@@ -4,35 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.IO;
 
 namespace MongoRepository
 {
     public class MongoConfiguration
     {
-
-        static Configuration configuration_;
+        static IConfigurationManager configuration_;
+       // static Configuration configuration_;
 
         static MongoConfiguration()
         {
-            configuration_ = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+           // var id=AppDomain.CurrentDomain.ApplicationIdentity;
+           // var configFile = Path.GetFileName( AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+
+            //configuration_ = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            configuration_ = ConfigurationManagerBuilder.Build();
         }
         public static MongoSection Section
         {
             get
             {
-                var section = ConfigurationManager.GetSection("mongodyn/mongo");
+                var section = configuration_.GetSection("mongodyn/mongo");
 
                 return (MongoSection)section;
             }
         }
 
-        public static Configuration Configuration
-        {
-            get
-            {
-                return configuration_;
-            }
-        }
+      
 
 
     }
